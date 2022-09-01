029b5a416c17cee6bbd88ebcba82bbec3a86bee7
Bondz <francesco.bondi02@gmail.com>
2022-09-01 20:16:26 +0200

    COmmit del client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = "https://beccaonline.herokuapp.com";
+axios.defaults.headers.common["X-Access-Token"] = localStorage.getItem("token");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
